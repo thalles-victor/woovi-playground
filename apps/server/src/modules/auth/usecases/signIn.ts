f3c91b50a65b951678a3d2eda2ce58941cb3f16f
thalles-victor <thalles.victor2012@gmail.com>
2025-05-02 11:11:02 -0300

    feat: add signIn use case

---
+++ apps/server/src/modules/auth/usecases/signIn.ts
@@ -0,0 +1,80 @@
+import JWT from "jsonwebtoken" 
+import { SignInDto, signInDtoSchema } from "./auth.dtos";
+import {CustomErrorResponse, validateSchema} from "../../error/validateSchema"
+import {UserModel} from "../../user/UserModel"
+import * as bcrypt from "bcrypt"
+import { PayloadType } from "../../../@shared/types";
+import {envParsed} from "../../../config"
+ 
+export async function  signIn(signInDto: SignInDto) {
+  validateSchema(signInDto, signInDtoSchema);
+
+  const userExist = await UserModel.findOne({
+    email: signInDto.email,
+  }).exec();
+
+  if (!userExist) {
+    throw new CustomErrorResponse({
+      message: "user not found",
+      statusCode: 404,
+    });
+  }
+
+  if (userExist.deletedAt) {
+    throw new CustomErrorResponse({
+      message: "user deleted or banned",
+      statusCode: 406,
+    });
+  }
+
+  if (!userExist.activatedAt) {
+    throw new CustomErrorResponse({
+      message: "require activate account",
+      statusCode: 406,
+    });
+  }
+
+  if (!userExist.password) {
+    throw new CustomErrorResponse({
+      message: "It is necessary to register a password",
+      statusCode: 406,
+    });
+  }
+
+  const passwordMatches = await bcrypt.compare(
+    signInDto.password,
+    userExist.password
+  );
+
+  if (!passwordMatches) {
+    throw new CustomErrorResponse({
+      message: "invalid password",
+      statusCode: 401,
+    });
+  }
+
+  const payload: PayloadType = {
+    sub: userExist._id.toString(),
+    role: userExist.role,
+  };
+
+  const token = JWT.sign(payload, envParsed.JWT_SECRET, {
+    expiresIn: "24h",
+  });
+
+  return {
+    user: {
+      name: userExist.name,
+      email: userExist.email,
+      createdAt: userExist.createdAt,
+      updatedAt: userExist.updatedAt,
+      deletedAt: userExist.deletedAt,
+      activatedAt: userExist.activatedAt,
+      role: userExist.role,
+    },
+    accessToken: {
+      token,
+      expiresIn: "24h",
+    },
+  };
+}
